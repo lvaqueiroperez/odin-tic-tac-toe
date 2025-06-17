18ff0e436678f28802c6ebee570005a8a2f276f5
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-06-17 12:02:33 +0200

    Add createPlayer factory function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("cacota");
+function createPlayer(name) {
+    let gamesWon = 0;
+    const giveWin = () => gamesWon++;
+    const getWins = () => gamesWon;
+    let boardSymbol = "";
+    const setBoardSymbol = (symbol) => boardSymbol = symbol;
+    const getBoardSymbol = () => boardSymbol;
+
+    return { giveWin, getWins, setBoardSymbol, getBoardSymbol, name };
+}
+
