27f73585714050f675dabc5ca18da8ef1963fd3b
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-06-20 11:21:58 +0200

    Improve readability

--- javascript.js
+++ javascript.js
@@ -18,7 +18,11 @@ const gameBoardModule = (function () {
 
     const displayBoard = function () {
         console.log(
-            `${board[0]}  ${board[1]}  ${board[2]}\n${board[3]}  ${board[4]}  ${board[5]}\n${board[6]}  ${board[7]}  ${board[8]}`
+            `
+            ${board[0]}  ${board[1]}  ${board[2]}\n
+            ${board[3]}  ${board[4]}  ${board[5]}\n
+            ${board[6]}  ${board[7]}  ${board[8]}
+            `
         );
     }
 
